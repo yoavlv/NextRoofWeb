0a59d13a8b437b8af244397db79d305794d1d0b6
yoavlv <yoavlv12@gmail.com>
2023-08-15 14:33:03 +0300

    update

--- static/js/asset_value.js
+++ static/js/asset_value.js
@@ -16,3 +16,27 @@ document.addEventListener('DOMContentLoaded', function() {
         streetSelect.value = selectedStreet;
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const buyElement = document.querySelector('.buy');
+    const saleElement = document.querySelector('.sale');
+
+    if (window.location.href.includes('asset_value')) {
+        saleElement.classList.add('active');
+    } else {
+        buyElement.classList.add('active');
+    }
+
+    buyElement.addEventListener('click', function() {
+        setActiveState(buyElement, saleElement);
+    });
+
+    saleElement.addEventListener('click', function() {
+        setActiveState(saleElement, buyElement);
+    });
+
+    function setActiveState(toActivate, toDeactivate) {
+        toActivate.classList.add('active');
+        toDeactivate.classList.remove('active');
+    }
+});
